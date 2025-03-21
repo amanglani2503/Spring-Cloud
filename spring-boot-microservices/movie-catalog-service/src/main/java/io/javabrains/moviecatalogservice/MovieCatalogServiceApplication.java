0445ee6be06b5ba5496e68bcc1f5ac6fbba26fd6
amanglani2503 <ambermanglani8871@gmail.com>
2025-03-22 00:00:57 +0530

    [Amber] Added Circuit breakers for fault tolerance and resilience

--- spring-boot-microservices/movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
+++ spring-boot-microservices/movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -3,12 +3,10 @@ package io.javabrains.moviecatalogservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableHystrix
 public class MovieCatalogServiceApplication {
 
 	public static void main(String[] args) {
